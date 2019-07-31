2682e4aa6e52a1a63cabfb3543fddcc639f428a1
thotakurasai <53036884+thotakurasai@users.noreply.github.com>
2019-07-31 17:55:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import logo from './logo.svg';
+import {Homepage} from './homepage/homepage';
+import {About} from './homepage/about';
+import {Users} from './homepage/users';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import axios from 'axios';
+
+import './App.css';
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+    usersdata: []
+    };
+    }
+  componentDidMount(){
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(res => {
+    this.setState({usersdata:res.data})
+    })
+  .catch(function (error) {
+  console.log(error);
+  })
+  }    
+    render(){return (
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+            <li>
+              <Link to="/users/">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Homepage} />
+        <Route path="/about/" component={About} />
+        <Route path="/users/" component={Users} />
+      </div>
+    </Router>
+      
+    );
+  }
+}
+
+export default App;
---
+++ users.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Users extends Component {
+
+    render() {
+        return (
+            <div>
+                i am users
+            </div>
+        )
+    }
+}
+
+export default Users
